ac490ff78e1b4f545242d406cff297199c6b527c
grigz44 <sarathkumarennissery2000@gmail.com>
2022-12-12 20:34:29 +0530

    s

--- src/Addexam.js
+++ src/Addexam.js
@@ -53,10 +53,10 @@ const Addexam = ({ onAdd }) => {
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-4">
-          <h3 className="float-left">Create a new Exam</h3>
+          <h3 className="float-left">Create a new Exams</h3>
           <Form onSubmit={onSubmit} className="mt-4">
             <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Label>{Examid}Exam_names</Form.Label>
+              <Form.Label>{Examid}Exam_name</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter Exam name"
